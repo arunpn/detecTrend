70d812193bdf0964a77be9191000f5125f93f173
Peter <peter.weir@gmail.com>
2016-09-15 13:57:46 -0700

    updated path to data

--- daily_1D.py
+++ daily_1D.py
@@ -6,7 +6,9 @@ from sklearn import linear_model
 import matplotlib.pyplot as plt
 plt.ion()
 
-vid_list_file = h5py.File('vid_list.hdf5', 'r')
+DATA_PATH = '../current/data/'
+
+vid_list_file = h5py.File('../current/vid_list.hdf5', 'r')
 vids = np.copy(vid_list_file['vids'])
 is_test = np.copy(vid_list_file['is_test'])
 vid_list_file.close()
@@ -21,20 +23,20 @@ x_train = np.zeros((len(train_vids), 1), dtype='int') - 1
 y_train = np.zeros((len(train_vids), 1), dtype='int') - 1
 
 for train_vid_ind, train_vid in enumerate(train_vids):
-    x_file_name = './data/' + train_vid + '.' + x_date_str + '.hdf5'
+    x_file_name = DATA_PATH + train_vid + '.' + x_date_str + '.hdf5'
     x_df = pd.read_hdf(x_file_name)
     account_array_this_video = x_df['account_id'].values
     num_unique_accounts = len(np.unique(account_array_this_video))
     
     x_train[train_vid_ind, 0] = num_unique_accounts
     
-    y_file_name = './data/' + train_vid + '.' + y_date_str + '.hdf5'
+    y_file_name = DATA_PATH + train_vid + '.' + y_date_str + '.hdf5'
     y_df = pd.read_hdf(y_file_name)
     account_array_this_video = y_df['account_id'].values
     num_unique_accounts = len(np.unique(account_array_this_video))
     
     y_train[train_vid_ind, 0] = num_unique_accounts
-    print(train_vid_ind, end='')
+    print(train_vid_ind, end=' ')
 
 x_train_mean, x_train_std = np.mean(x_train, axis=0), np.std(x_train, axis=0)
 y_train_mean, y_train_std = np.mean(y_train, axis=0), np.std(y_train, axis=0)
@@ -50,7 +52,7 @@ ax_train = fig.add_subplot(2, 1, 1)
 ax_train.plot(x_train_z, y_train_z, '.')
 ax_train.plot(x_train_z, regr.predict(x_train_z), color='blue', linewidth=3)
 
-print('Coefficients: ', regr.coef_)
+print('\nCoefficients: ', regr.coef_)
 
 ######################################## test #########################
 test_vids = vids[is_test==True]
@@ -58,26 +60,26 @@ x_test = np.zeros((len(test_vids), 1), dtype='int') - 1
 y_test = np.zeros((len(test_vids), 1), dtype='int') - 1
 
 for test_vid_ind, test_vid in enumerate(test_vids):
-    x_file_name = './data/' + test_vid + '.' + x_date_str + '.hdf5'
+    x_file_name = DATA_PATH + test_vid + '.' + x_date_str + '.hdf5'
     x_df = pd.read_hdf(x_file_name)
     account_array_this_video = x_df['account_id'].values
     num_unique_accounts = len(np.unique(account_array_this_video))
     
     x_test[test_vid_ind, 0] = num_unique_accounts
     
-    y_file_name = './data/' + test_vid + '.' + y_date_str + '.hdf5'
+    y_file_name = DATA_PATH + test_vid + '.' + y_date_str + '.hdf5'
     y_df = pd.read_hdf(y_file_name)
     account_array_this_video = y_df['account_id'].values
     num_unique_accounts = len(np.unique(account_array_this_video))
     
     y_test[test_vid_ind, 0] = num_unique_accounts
-    print(test_vid_ind, end='')
+    print(test_vid_ind, end=' ')
 
 x_test_z = (x_test - x_train_mean)/x_train_std
 y_test_z = (y_test - y_train_mean)/y_train_std
 
 # The mean square error
-print("Residual sum of squares: %.2f"
+print("\nResidual sum of squares: %.2f"
       % np.mean((regr.predict(x_test_z) - y_test_z) ** 2))
 # Explained variance score: 1 is perfect prediction
 print('Variance score: %.2f' % regr.score(x_test_z, y_test_z))
