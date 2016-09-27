8371e555c1bb159c003a489b1ab7e10acacdf906
Peter <peter.weir@gmail.com>
2016-09-27 08:41:33 -0700

    predicting if in top 5 of not

--- predict.py
+++ predict.py
@@ -6,8 +6,9 @@ from sklearn import linear_model, metrics
 import matplotlib.pyplot as plt
 plt.ion()
 import os
-
-from sklearn import svm, tree
+plt.rcParams['svg.fonttype'] = 'none'
+plt.rcParams['font.size'] = 24
+#from sklearn import svm, tree
 from sklearn.ensemble import RandomForestClassifier
 
 def get_vid_list(data_path):
@@ -16,10 +17,15 @@ def get_vid_list(data_path):
     return vids
 
 def get_vid_published_dates(vids):
-    published_date_df = pd.read_hdf('../vid_published_dates.hdf5')
+    #published_date_df = pd.read_hdf('../vid_published_dates.hdf5')
+    published_date_df = pd.read_hdf('../vid_published_datetimes.hdf5')
     vid_dates = []
     for vid in vids:
-        this_vid_d = published_date_df.datetime[published_date_df.vid==vid].values[0]
+        if vid in published_date_df.vid.values:
+            this_vid_d = published_date_df.datetime[published_date_df.vid==vid].values[0]
+        else:
+            print('no datetime for ' + vid)
+            this_vid_d = pd.to_datetime('')
         vid_dates.append(this_vid_d)
     vid_dates = np.array(vid_dates)
     return vid_dates
@@ -92,7 +98,11 @@ def read_data(vids, data_path, t_division):
     t = sum_hourly_viewers.index
     return vids_out, t, account_data, org_data
 
-def plot_peak_triggered(x):
+def plot_peak_triggered(x_in, num_to_choose):
+    most_popular_hourly = np.argsort(x_in, 0)[-num_to_choose:, :]
+    most_popular = np.unique(most_popular_hourly)
+    
+    x = x_in[most_popular, :]
     mask_x = np.ma.masked_all_like(x)
     mask_x.fill(np.nan)
     padded_x = np.ma.concatenate((mask_x, x, mask_x), axis=1)
@@ -106,7 +116,7 @@ def plot_peak_triggered(x):
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.set_xlim(-5, 10)
+
     upper = np.nanpercentile(xx, 75, axis=0)
     lower = np.nanpercentile(xx, 25, axis=0)
     hour_range = np.arange(xx.shape[1]) - xx.shape[1]/2.
@@ -119,12 +129,12 @@ def plot_peak_triggered(x):
     ax.set_xlabel('Hours after peak', fontname='FreeSans')
     ax.set_ylabel('Number of viewers', fontname='FreeSans')
     
-    ax.text(hour_range[hours_to_plot][-1]+.5, np.ma.mean(xx, 0)[hours_to_plot][-1], 'Mean', color='b')
-    ax.text(hour_range[hours_to_plot][-1]+.5, np.ma.median(xx, 0)[hours_to_plot][-1], 'Median', color='k')
+    ax.text(hour_range[hours_to_plot][-1]+.5, np.ma.mean(xx, 0)[hours_to_plot][-1]+.5, 'Mean', color='b')
+    ax.text(hour_range[hours_to_plot][-1]+.5, np.ma.median(xx, 0)[hours_to_plot][-1]-.5, 'Median', color='k')
 
-    ax.spines['left'].set_bounds(0, 300)
-    ax.set_ylim((-20, 350))
-    ax.set_yticks([0, 100, 200, 300])
+    ax.spines['left'].set_bounds(0, 30)
+    ax.set_ylim((-2, 37))
+    ax.set_yticks([0, 10, 20, 30])
 
     ax.spines['bottom'].set_bounds(-5, 10)
     ax.set_xlim((-6, 11))
@@ -137,7 +147,7 @@ def plot_peak_triggered(x):
     ax.xaxis.set_ticks_position('bottom')
     ax.yaxis.set_ticks_position('left')
 
-    fig.savefig('peak_triggered.svg')
+    #fig.savefig('peak_triggered.svg')
     return fig
 
 def calc_sum_watching_top(data, num_to_choose):
@@ -151,25 +161,39 @@ def calc_sum_watching_top(data, num_to_choose):
 def plot_time_series(t, x, sum_watching_top, num_to_choose):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.plot(t, x.sum(0), color=[.8, .8, .8])
-    ax.plot(t, sum_watching_top, color=[0, 0, 0])
+    ax.plot(t, x.sum(0), color=[.7, .7, .7], zorder=10)
+    ax.plot(t, sum_watching_top, color=[0, 0, 0], zorder=10)
+    ax.axvspan(pd.Timestamp('2016-07-13').toordinal(), pd.Timestamp('2016-07-27').toordinal(), zorder=1, color=[.8, .8, 1.])
     ax.spines['top'].set_visible(False)
     ax.xaxis.set_ticks_position('bottom')
     ax.set_ylabel('Number of viewers', color='k')
-
+    ax.set_yticks([0, 500, 1000, 1500])
+    ax.spines['left'].set_bounds(0, 1500)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_position(('outward', 10))
+    
     ax2 = ax.twinx()
     ax2.plot(t, 100*sum_watching_top/x.sum(0), color='r')
     ax2.set_ylabel('Watching top ' + str(num_to_choose) + ' (%)', color='r')
+    ax2.set_yticks([0, 25, 50, 75, 100])
     for tl in ax2.get_yticklabels():
         tl.set_color('r')
-    ax2.set_ylim((0, 100))
+    
+    ax2.spines['left'].set_visible(False)
     ax2.spines['top'].set_visible(False)
     ax2.spines['right'].set_color('r')
-
-    ax.set_xlim(pd.Timestamp('2016-06-14'), pd.Timestamp('2016-07-28'))
-    ax.text(pd.Timestamp('2016-06-17'), x.sum(0)[0]+2000, 'All', color=[.8, .8, .8])
-    ax.text(pd.Timestamp('2016-06-14'), sum_watching_top[0]-500, 'Top 5', color='k')
-    fig.savefig('time_series.svg')
+    
+    ax.spines['bottom'].set_bounds(pd.Timestamp('2016-06-18').toordinal(), pd.Timestamp('2016-07-23').toordinal())
+    ax2.spines['bottom'].set_bounds(pd.Timestamp('2016-06-18').toordinal(), pd.Timestamp('2016-07-23').toordinal())
+    
+    ax.set_xlim(pd.Timestamp('2016-06-17T12'), pd.Timestamp('2016-07-28'))
+    ax.text(pd.Timestamp('2016-07-28').toordinal(), x.sum(0)[-1], 'Top 100', color=[.7, .7, .7])
+    ax.text(pd.Timestamp('2016-07-28').toordinal(), sum_watching_top[-1], 'Top 5', color='k')
+    labels = ax.get_xticklabels()
+    plt.setp(labels, rotation=-30, ha='left')
+    ax2.spines['bottom'].set_position(('outward', 10))
+    
+    #fig.savefig('time_series.svg')
     return fig
 
 def extract_features(account_data, org_data, vid_dates, t_all, num_to_choose):
@@ -178,6 +202,7 @@ def extract_features(account_data, org_data, vid_dates, t_all, num_to_choose):
     num_vids = norm_account_data.shape[0]
     num_hours = norm_account_data.shape[1]
     x_tm1 = np.zeros((2*num_to_choose, num_hours), dtype='float') # will hold viewers from time t-1
+    x_tm2 = np.zeros((2*num_to_choose, num_hours), dtype='float') # will hold viewers from time t-1
     dx_tm1 = np.zeros((2*num_to_choose, num_hours), dtype='float') # will hold change of viewers between time t-1 and t-2
     xo_tm1 = np.zeros((2*num_to_choose, num_hours), dtype='float') # will hold orgs from time t-1
     xt_tm1 = np.zeros((2*num_to_choose, num_hours), dtype='float') # will hold time from published from time t-1
@@ -187,10 +212,15 @@ def extract_features(account_data, org_data, vid_dates, t_all, num_to_choose):
 
     for hour_ind in range(2, num_hours):
         for rank_ind in range(1, 2*num_to_choose+1):
-            #if rank_ind > num_to_choose:
-            #    rand_rank_ind = np.random.randint(rank_ind, 10)
-            vid_ind_this_hour = ix[-rank_ind, hour_ind]
+            if rank_ind > num_to_choose:
+                sample_rank_ind = np.random.randint(rank_ind, num_vids)
+                while norm_account_data[ix[-sample_rank_ind, hour_ind], hour_ind-1] == 0:
+                    sample_rank_ind = np.random.randint(rank_ind, num_vids)
+            else:
+                sample_rank_ind = rank_ind
+            vid_ind_this_hour = ix[-sample_rank_ind, hour_ind]
             x_tm1[rank_ind-1, hour_ind] = norm_account_data[vid_ind_this_hour, hour_ind-1]
+            x_tm2[rank_ind-1, hour_ind] = norm_account_data[vid_ind_this_hour, hour_ind-2]
             dx_tm1[rank_ind-1, hour_ind] = norm_account_data[vid_ind_this_hour, hour_ind-1] - norm_account_data[vid_ind_this_hour, hour_ind-2]
             xo_tm1[rank_ind-1, hour_ind] = norm_org_data[vid_ind_this_hour, hour_ind-1]
             if pd.isnull(vid_dates[vid_ind_this_hour]):
@@ -201,27 +231,37 @@ def extract_features(account_data, org_data, vid_dates, t_all, num_to_choose):
             y_t[rank_ind-1, hour_ind] = norm_account_data[vid_ind_this_hour, hour_ind]
         
     x_tm1 = x_tm1[:, 2:]
+    x_tm2 = x_tm2[:, 2:]
     dx_tm1 = dx_tm1[:, 2:]
     xo_tm1 = xo_tm1[:, 2:]
     xt_tm1 = xt_tm1[:, 2:]
     y_t = y_t[:, 2:]
 
     x_tm1 = x_tm1.reshape(2*num_to_choose*(num_hours - 2), 1)
+    x_tm2 = x_tm2.reshape(2*num_to_choose*(num_hours - 2), 1)
     dx_tm1 = dx_tm1.reshape(2*num_to_choose*(num_hours - 2), 1)
     xo_tm1 = xo_tm1.reshape(2*num_to_choose*(num_hours - 2), 1)
     xt_tm1 = xt_tm1.reshape(2*num_to_choose*(num_hours - 2), 1)
     xt_tm1[np.isnan(xt_tm1)] = np.nanmean(xt_tm1)
     y_t = y_t.reshape(2*num_to_choose*(num_hours - 2), 1)
 
-    #x_out = np.concatenate((x_tm1, dx_tm1), axis=1)
-    #x_out = np.concatenate((x_tm1, dx_tm1, xo_tm1, xt_tm1), axis=1)
-    x_out = np.concatenate((x_tm1, dx_tm1, xt_tm1), axis=1)
+    x_out = np.concatenate((x_tm1, dx_tm1, xo_tm1, xt_tm1), axis=1)
+
+    #x_out = x_tm1
+    #x_out = x_tm2
+    #x_out = xo_tm1
+    #x_out = dx_tm1
+    #x_out = xt_tm1
 
     y_out = y_t
     
     return x_out, y_out
     
 def viewers2score(y, num_hours, num_to_choose):
+    """example:
+    y = np.random.random_integers(0, 5, (6, 10))
+    y_score = viewers2score(y.ravel(), y.shape[1] + 2, y.shape[0]/2)
+    """
     hourly_y = y.reshape(2*num_to_choose, num_hours - 2)
     score = np.zeros_like(hourly_y)
 
@@ -230,6 +270,11 @@ def viewers2score(y, num_hours, num_to_choose):
         uh = np.sort(np.unique(h)).tolist()[::-1]
         sc = num_to_choose - np.array([uh.index(hh) for hh in h])
         sc = np.clip(sc, 0, num_to_choose)
+        while np.sum(sc > 0) > num_to_choose:
+            # randomly eliminate low scores
+            ind = np.argmax(sc == sc[sc>0].min())
+            sc[ind] = 0
+        
         score[:, hour_ind] = sc
     
     return score
@@ -245,6 +290,7 @@ def zero_outside_top(account_data, org_data, full_set_size):
         org_data_out[below_thesh_inds, hour_ind] = 0.
         
         while np.sum(account_data_out[:, hour_ind] > 0) > full_set_size:
+            # randomly eliminate lower viewership videos
             ind = np.argmax(account_data_out[:, hour_ind] == thesh_this_hour)
             account_data_out[ind, hour_ind] = 0
             org_data_out[ind, hour_ind] = 0
@@ -257,95 +303,79 @@ t_division = "h"
 
 all_vids = get_vid_list(DATA_PATH)
 vids_out, t_all, account_data_all, org_data_all = read_data(all_vids, DATA_PATH, t_division)
-full_set_size = 100
-account_data_all, org_data_all = zero_outside_top(account_data_all, org_data_all, full_set_size)
-fig_peak_triggered = plot_peak_triggered(account_data_all)
-sum_watching_top = calc_sum_watching_top(account_data_all, num_to_choose)
-fig_time_series = plot_time_series(t_all, account_data_all, sum_watching_top, num_to_choose)
+
+#fig_peak_triggered = plot_peak_triggered(account_data_all, num_to_choose)
+#sum_watching_top = calc_sum_watching_top(account_data_all, num_to_choose)
+#account_data_all_top100, org_data_all_top100 = zero_outside_top(account_data_all, org_data_all, 100)
+#fig_time_series = plot_time_series(t_all, account_data_all_top100, sum_watching_top, num_to_choose)
 
 vid_dates = get_vid_published_dates(vids_out)
 
 num_hours_all = account_data_all.shape[1]
 
-is_test_hour = np.arange(num_hours_all) >= num_hours_all - 7*24
-is_train_hour = np.arange(num_hours_all) < num_hours_all - 7*24
+for data_set in ['train', 'test']:
+    if data_set == 'train':
+        is_in_set_hour = np.arange(num_hours_all) < num_hours_all - 2*7*24
+    elif data_set == 'test':
+        is_in_set_hour = np.arange(num_hours_all) >= num_hours_all - 2*7*24
 
-num_hours_test = is_test_hour.sum()
-num_hours_train = is_train_hour.sum()
+    num_hours_set = is_in_set_hour.sum()
 
-account_data_train = np.copy(account_data_all[:, is_train_hour])
-org_data_train = np.copy(org_data_all[:, is_train_hour])
-x_train, y_train = extract_features(account_data_train, org_data_train, vid_dates, t_all, num_to_choose)
-
-x_train_mean, x_train_std = np.mean(x_train, axis=0), np.std(x_train, axis=0)
-y_train_mean, y_train_std = np.mean(y_train, axis=0), np.std(y_train, axis=0)
-x_train_z = x_train
-y_train_z = y_train
-#x_train_z = (x_train - x_train_mean)/x_train_std
-#y_train_z = (y_train - y_train_mean)/y_train_std
-
-regr = linear_model.LinearRegression()
-#regr.fit_intercept = False
-regr.fit(x_train_z, y_train_z)
-
-print('Coefficients: ', regr.coef_)
-print('Variance score training set: %.2f' % regr.score(x_train_z, y_train_z))
-
-predicted_y_train_z = regr.predict(x_train_z)
-predicted_y_train = predicted_y_train_z
-#predicted_y_train = predicted_y_train_z*y_train_std + y_train_mean
-
-fig_train = plt.figure()
-ax_train0 = fig_train.add_subplot(2, 1, 1)
-ax_train0.plot(y_train, predicted_y_train, 'b.')
-#ax_train0.plot((0, 2300), (0, 2300), 'k:')
-ax_train1 = fig_train.add_subplot(2, 1, 2)
-ax_train1.plot(np.sqrt(y_train), np.sqrt(predicted_y_train), 'b.')
-#ax_train1.plot((0, np.sqrt(2300)), (0, np.sqrt(2300)), 'k:')
-
-score_train = viewers2score(y_train, num_hours_train, num_to_choose)
-predicted_score_train = viewers2score(predicted_y_train, num_hours_train, num_to_choose)
-
-hourly_rmse = np.sqrt(np.mean((score_train[:num_to_choose, :] - predicted_score_train[:num_to_choose, :])**2, axis=0))
-hourly_sae = np.sum(np.abs(score_train[:num_to_choose, :] - predicted_score_train[:num_to_choose, :]), axis=0)/float(sum(range(num_to_choose+1)))
-hourly_sae = np.sum(np.abs(score_train), axis=0)
-
-
-######################################## test #########################
-
-account_data_test = np.copy(account_data_all[:, is_test_hour])
-org_data_test = np.copy(org_data_all[:, is_test_hour])
-x_test, y_test = extract_features(account_data_test, org_data_test, vid_dates, t_all, num_to_choose)
-
-x_test_z = x_test
-y_test_z = y_test
-#x_test_z = (x_test - x_train_mean)/x_train_std
-#y_test_z = (y_test - y_train_mean)/y_train_std
-
-# Explained variance score: 1 is perfect prediction
-print('Variance score test set: %.2f' % regr.score(x_test_z, y_test_z))
+    account_data = np.copy(account_data_all[:, is_in_set_hour])
+    org_data = np.copy(org_data_all[:, is_in_set_hour])
+    x, y = extract_features(account_data, org_data, vid_dates, t_all, num_to_choose)
     
-predicted_y_test_z = regr.predict(x_test_z)
-predicted_y_test = predicted_y_test_z
-#predicted_y_test = predicted_y_test_z*y_train_std + y_train_mean
+    y_score = viewers2score(y, num_hours_set, num_to_choose) > 0
+    
+    if data_set == 'train':
+        figROC = plt.figure()
+        axROC = figROC.add_subplot(111)
+        regr = linear_model.LinearRegression()
+        regr.fit(x, y)
+        
+        figRaw = plt.figure()
+        axRaw = figRaw.add_subplot(111)
 
-fig_test = plt.figure()
-ax_test0 = fig_test.add_subplot(2, 1, 1)
-ax_test0.plot(y_test, predicted_y_test, 'b.')
-#ax_test0.plot((0, 2300), (0, 2300), 'k:')
-ax_test1 = fig_test.add_subplot(2, 1, 2)
-ax_test1.plot(np.sqrt(y_test), np.sqrt(predicted_y_test), 'b.')
-#ax_test1.plot((0, np.sqrt(2300)), (0, np.sqrt(2300)), 'k:')
+    
+    print('Coefficients: ', regr.coef_)
+    print('Variance score '+data_set+' set: %.2f' % regr.score(x, y))
+    
+    predicted_y = regr.predict(x)
+    predicted_y[x[:, 0]==0] = 0 # otherwise unfair selection bias
+    
+    #predicted_y = x
+    predicted_y_score = viewers2score(predicted_y, num_hours_set, num_to_choose) > 0
+    
+    print('LR Accuracy score '+data_set+' set: %.2f' % metrics.accuracy_score(y_score.ravel(), predicted_y_score.ravel()))
+    
+    f, t, th = metrics.roc_curve(y_score.ravel(), predicted_y)
+    axROC.plot(f, t, label=data_set+'Linear regression')
+    
+    axRaw.plot(x[y_score.ravel()==0, 0].ravel(), y[y_score.ravel()==0].ravel(), '.', label=data_set+' True negatives')
+    axRaw.plot(x[y_score.ravel()>0, 0].ravel(), y[y_score.ravel()>0].ravel(), '.', label=data_set+' True positives')
+    #if data_set == 'test':
+    #    axRaw.plot(x[y_score.ravel()==0, 0].ravel(), x[y_score.ravel()==0, 1].ravel(), '.k', label=data_set+' True negatives')
+    #    axRaw.plot(x[y_score.ravel()>0, 0].ravel(), x[y_score.ravel()>0, 1].ravel(), '.r', label=data_set+' True positives')
+    
+    if data_set == 'train':
+        #clf = svm.SVC()
+        #clf = tree.DecisionTreeClassifier()
+        clf = RandomForestClassifier(max_depth=4)
+        clf.fit(x, y_score.ravel())
+        
+    #predicted_y = clf.decision_function(x)
+    predicted_y = clf.predict_proba(x)[:, 1]
+    predicted_y[x[:, 0]==0] = 0 # otherwise unfair selection bias
+    predicted_y_score = viewers2score(predicted_y, num_hours_set, num_to_choose) > 0
+    print('RF Accuracy score '+data_set+' set: %.2f' % metrics.accuracy_score(y_score.ravel(), predicted_y_score.ravel()))
 
-figROC = plt.figure()
-axROC = figROC.add_subplot(111)
-f, t, th = metrics.roc_curve(y_test_z>0, regr.predict(x_test_z))
-axROC.plot(f, t)
+    f, t, th = metrics.roc_curve(y_score.ravel(), predicted_y.ravel())
+    axROC.plot(f, t, ':', label=data_set+' Random forest')
+    
+    axRaw.plot(x[:, 0], predicted_y, '.', label=data_set+' Random forest')
+    
+    
 
-score_test = viewers2score(y_test, num_hours_test, num_to_choose)
-predicted_score_test = viewers2score(predicted_y_test, num_hours_test, num_to_choose)
-#hourly_sae = np.sum(np.abs(score_test[:num_to_choose, :] - predicted_score_test[:num_to_choose, :]), axis=0)/float(sum(range(num_to_choose+1)))
-hourly_sae = np.sum(np.abs(score_test[:num_to_choose, :] - predicted_score_test[:num_to_choose, :]), axis=0)/float(sum(range(num_to_choose+1)))
-plt.figure()
-plt.hist(hourly_sae, num_to_choose)
-print(np.mean(hourly_sae))
+axROC.legend(loc='lower right')
+axRaw.legend(loc='lower right')
+
